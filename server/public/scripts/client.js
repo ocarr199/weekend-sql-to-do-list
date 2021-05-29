38bf5167e7069bbfe7c6e6fdd49fb548d5733acf
Owen Carruth <ocarr199@gmail.com>
2021-05-29 15:03:01 -0500

    Put working

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -6,6 +6,7 @@ $(document).ready(function () {
   // load to-do list on page load
   getTasks();
   $('#addTaskButton').on('click', postTask)
+  $('#showTasks').on('click', '.completedButton', handleComplete)
 });
 
 
@@ -19,6 +20,18 @@ function getTasks() {
   }).then( response => {
     // console log the response
     console.log(response);
+    $('#showTasks').empty();
+    for(let task of response){
+      console.log(task);
+      $('#showTasks').append(`
+      <tr> 
+          <td> ${task.task}</td>
+          <td><button class="completedButton" data-id="${task.id}">Completed</button></td>
+          <td><button class="deleteButton" data-id="${task.id}">Delete</button></td>
+        </tr>
+      `)
+    }
+ 
   }).catch( err => {
     // console log the error
     console.log('Error in GET', err);
@@ -32,7 +45,7 @@ function postTask() {
   //post task to server
   // create new task object
   newTask = {
-    task: $('addTask').val()
+    task: $('#taskInp').val()
   }
 
   $.ajax({
@@ -46,6 +59,25 @@ function postTask() {
       console.log('Error in POST', error)
       alert('Unable to add new Task');
     });
+}
+
+function handleComplete(){
+  let id = $(this).data('id');
+  completedTask(id);
+}
+
+function completedTask(taskId){
+  $.ajax({
+    method: 'PUT',
+    url: `./tasks/${taskId}`
+  }).then( (response) => {
+    console.log('task completed update:', response);
+    getTasks();
+    console.log("completed");
+}).catch(err =>{
+    console.log('transfer was not updated');
+    alert('there was an error with updating')
+})
 }
 
 
--- server/routes/todo.router.js
+++ server/routes/todo.router.js
@@ -14,6 +14,7 @@ taskRouter.get('/', (req, res) => {
     .then( result => {
         // send the results in an object
         res.send(result.rows);
+      
     })
     .catch( error => {
         console.log('Error getting tasks', error);
@@ -41,6 +42,23 @@ taskRouter.post('/',  (req, res) => {
       });
   });
 //   END POST
+
+// PUT
+taskRouter.put('/:id', (req,res)=>{
+  const taskCompleteUpdate = req.params.id;
+  console.log("task Completed", taskCompleteUpdate);
+  const queryString = `UPDATE "tasks" SET "completed"=NOT "completed" WHERE "tasks".id = $1;`; 
+
+  pool.query(queryString,[taskCompleteUpdate])
+  .then( response => {
+    console.log(response);
+    console.log(`completed task with id ${taskCompleteUpdate}`);
+    res.sendStatus(200); // confirms on client side that info updated
+}).catch((err) => {
+    console.log('error in server', err);
+    res.sendStatus(500); // shows error on this server route
+})
+})
 
 
 module.exports = taskRouter;
